cc61482d193935dd9a4ecc55721b7fac999ee777
Shreeyash <shreeyash545@gmail.com>
2022-11-28 22:02:11 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const date=new Date();
+const year=date.getFullYear();
+
+function Footer(){
+    return(
+        <footer>
+            <p>Copyright ©{year} SHREEYASH</p>
+        </footer>
+    );
+}
+
+export default Footer;
