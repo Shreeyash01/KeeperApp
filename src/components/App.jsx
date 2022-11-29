8b296bc01b5a3fe49eec8e26d1fbc738417cc51d
Shreeyash <shreeyash545@gmail.com>
2022-11-29 18:52:36 +0530

    Added more Notes using Map and Arrow functions of React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App(){
-    return(
+// function newNotes(note){
+//     return(
+//         <Note 
+//             key={note.key}
+//             title={note.title}
+//             content={note.content}
+//         />
+//     );
+// }
+
+function App() {
+    return (
         <div>
             <Header />
-            <Note />
+            <Note
+                title="First Note"
+                content="This is the first Note."
+            />
+            {/* {notes.map( (note) =>
+                (
+                    <Note
+                        key={note.key}
+                        title={note.title}
+                        content={note.content}
+                    />
+                )
+            )}; */}
+
+            {notes.map( note => (<Note key={note.key} title={note.title} content={note.content} /> ))}
+
             <Footer />
-        </div>   
+        </div>
     );
 }
 
