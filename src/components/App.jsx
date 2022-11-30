b54922c78a85745c5a87534566f45d7ecc8a4a30
Shreeyash <shreeyash545@gmail.com>
2022-11-30 19:15:35 +0530

    Added a functionality for dynamically creating and deleting new Notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import notesA from "../notesA";
+import CreateArea from "./CreateArea";
+
 
 // function newNotes(note){
 //     return(
@@ -15,24 +17,45 @@ import notes from "../notes";
 // }
 
 function App() {
+    const [notes,setNotes]=useState([]);
+    
+    function addNote(newNote){
+        setNotes(prevNotes=>{
+            return [...prevNotes,newNote];
+        })
+    }
+    function deleteNote(id){
+        setNotes(prevNotes=>{
+            return prevNotes.filter((noteItem,index)=>{
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
+
+            <CreateArea onAdd={addNote} />
+            
+            {notes.map( (noteItem,index) =>{
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
+
             <Note
                 title="First Note"
                 content="This is the first Note."
             />
-            {/* {notes.map( (note) =>
-                (
-                    <Note
-                        key={note.key}
-                        title={note.title}
-                        content={note.content}
-                    />
-                )
-            )}; */}
 
-            {notes.map( note => (<Note key={note.key} title={note.title} content={note.content} /> ))}
+            {notesA.map( note => (<Note key={note.key} title={note.title} content={note.content} /> ))}
 
             <Footer />
         </div>
--- src/notesA.js
+++ src/notesA.js
@@ -1,4 +1,4 @@
-const notes = [
+const notesA = [
     {
         key: 1,
         title: "Delegation",
@@ -25,4 +25,4 @@ const notes = [
     }
 ];
 
-export default notes;
+export default notesA;
